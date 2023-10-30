7c0e44b29aeb2b945e1d6626da814aea6d99a8f2
CookiesOfficial <teamtyler2@gmail.com>
2023-10-29 23:27:46 -0600

    Blackout Command Basic

---
+++ Cookies/Cookies/Commands/Blackout.cs
@@ -0,0 +1,30 @@
+﻿using CommandSystem;
+using System;
+using PluginAPI.Core;
+
+namespace Cookies.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    class Blackout : ICommand
+    {
+        public string Command => "blackout";
+        public string[] Aliases { get; } = { "cookies" };
+
+        public string Description => "Blackout The Entire Faculty";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!sender.CheckPermission(PlayerPermissions.SetGroup))
+            {
+                response = "System Administrator Only Command";
+                return false;
+            }
+            else
+            {
+                Facility.TurnOffAllLights();
+                response = "All Lights Have Been Turned OFF!";
+                return true;
+            }
+        }
+    }
+}
---
+++ Cookies/Cookies/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace Cookies
+{
+    public class MainClass
+    {
+        public const string Author = "Cookies";
+        public const string Name = "Cookies";
+        public const string Version = "1.0.0";
+        public const string Description = "Cookies Random Plugins";
+
+        [PluginEntryPoint(Name, Version, Description, Author)]
+        public void OnEnabled()
+        {
+            Log.Info("Cookies plugin loaded. Written by Cookies");
+        }
+    }
+}
